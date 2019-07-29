0e902e3dc705afabeb39ad75cbf69c11d5c26cbc
Rob Wetherall <robertgwetherall1@gmail.com>
2019-07-29 06:25:10 -0400

    trying again

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ async function start() {
 	})
 
 	server.applyMiddleware({
-		app
-		// cors: {
-		// 	credentials: true,
-		// 	origin: 'https://mrwetherall.org'
-		// }
+		app,
+		cors: {
+			credentials: true,
+			origin: 'https://mrwetherall.org'
+		}
 	})
 
 	app.get('/', (req, res) => res.end(`Teacher's Aide API`))
