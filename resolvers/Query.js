dcf957bace620da9bf1954be6f8c0ea82200c666
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-05 06:36:56 -0400

    loggin the me session userID

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -10,6 +10,7 @@ module.exports = {
 	allUsers: (_, __, { users }) => users.find().toArray(),
 
 	me: (_, __, { req, users }) => {
+		console.log(req.session.userID)
 		if (!req.session.userId) {
 			return null
 		}
