804fec9bd203621a303000a0c93c85f97eba3b44
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-08 11:13:30 -0400

    forgot traling comma

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function start() {
 			secret: process.env.SESSION_SECRET,
 			resave: false,
 			cookie: {
-				maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+				maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
 				// secure: true
 				path: '/'
 			},
