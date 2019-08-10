9e3e588f6f34062efc76c15cd4005fb924f43ffb
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-10 12:08:49 -0400

    secure: true is back

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ async function start() {
 			resave: false,
 			cookie: {
 				maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-				path: '/dashboard'
+				path: '/',
+				secure: true
 			},
 			store: store,
 			saveUninitialized: false
