ba9f3dc5031e30a4f799f04b19b035cf40cd310e
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-15 14:17:58 -0400

    w/out createClassPeriod mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -135,11 +135,11 @@ module.exports = {
 			removed = true
 		}
 		return { removed, lesson }
-	},
-
-	async createClassPeriod(_, { ...args }, { classPeriodData }) {
-		console.log(args)
-		let newClassPeriod
-		return { dateAssigned, classPeriod }
 	}
+
+	// async createClassPeriod(_, { ...args }, { classPeriodData }) {
+	// 	console.log(args)
+	// 	let newClassPeriod
+	// 	return { dateAssigned, classPeriod }
+	// }
 }
