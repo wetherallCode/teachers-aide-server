60be090fd48a5cc21b87d5bfac44a5a568c4d6f2
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-24 15:07:22 -0400

    without

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -218,14 +218,15 @@ module.exports = {
 			}
 		)
 		const updatedStudent = await studentData.findOne({ _id: ObjectID(_id) })
-		const addAbsentStudentToClassPeriod = await classPeriodData.updateOne(
-			{ assignedDate, period },
-			{
-				$push: {
-					absentStudents: updatedStudent
-				}
-			}
-		)
-		return { updatedStudent, addAbsentStudentToClassPeriod }
+
+		// const addAbsentStudentToClassPeriod = await classPeriodData.updateOne(
+		// 	{ assignedDate, period },
+		// 	{
+		// 		$push: {
+		// 			absentStudents: updatedStudent
+		// 		}
+		// 	}
+		// )
+		return { updatedStudent }
 	}
 }
