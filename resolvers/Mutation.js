4ed52ae5e5954db3a09a265996878d3476e199a8
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-25 14:10:13 -0400

    figuring out why marStudentAbsent doesn't work

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -209,12 +209,8 @@ module.exports = {
 		{ _id, date, assignedDate, period },
 		{ studentData, classPeriodData }
 	) {
-		console.log(_id)
-		console.log(assignedDate)
-		console.log(date)
-		console.log(period)
 		const findStudentAbsences = await studentData.findOne({ _id: ObjectID(_id) })
-		console.log(findStudentAbsences)
+		console.log(findStudentAbsences.daysAbsent)
 		const checkForDuplicateAbsences = findStudentAbsences.daysAbsent.find(
 			element => element === date
 		)
