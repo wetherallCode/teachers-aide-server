e139b24182dffe7fad3277e9a75d2f5c12e06ca7
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-25 06:59:03 -0400

    this could work

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -210,7 +210,7 @@ module.exports = {
 		{ studentData, classPeriodData }
 	) {
 		const findStudentAbsences = await studentData.findOne({ _id: ObjectID(_id) })
-		console.log(findStudentAbsences.daysAbsent)
+		console.log(findStudentAbsences.daysAbsent.find(element => element === date))
 		// const checkForDuplicates = findStudentAbsences.find(element => element === date)
 		// console.log(checkForDuplicates)
 		// if (checkForDuplicates.daysAbsent === date) {
