14cf1c681679542d934e1bdfe71a861c0132bc88
Rob Wetherall <robertgwetherall1@gmail.com>
2020-01-16 11:18:06 -0500

    added markingPeriod feild to AssignedHomework

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -153,7 +153,6 @@ module.exports = {
 	},
 
 	async createUnit(_, args, { unitData }) {
-		console.log(args)
 		let newUnit = {
 			...args.input
 		}
@@ -200,6 +199,7 @@ module.exports = {
 							assignmentType: assignment.assignmentType,
 							assignedDate: assignment.assignedDate,
 							dueDate: assignment.dueDate,
+							markingPeriod: assignment.markingPeriod,
 							readingPages: assignment.readingPages,
 							readingSections: assignment.readingSections,
 							missing: true,
