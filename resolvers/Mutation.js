1a360ca2d45d24c4fd97cbca3244ded440af3ec3
Rob Wetherall <robertgwetherall1@gmail.com>
2020-01-28 20:35:42 -0500

    logging

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -197,7 +197,7 @@ module.exports = {
 			assignedHomework,
 			assignedTest
 		}
-
+		console.log(assignedTest)
 		const { insertedId } = await classPeriodData.insertOne(newClassPeriod)
 		newClassPeriod._id = insertedId
 
