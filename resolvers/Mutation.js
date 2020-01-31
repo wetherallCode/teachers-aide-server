bb4e98916fa058388cdad4e89c44528cbd83cd66
Rob Wetherall <robertgwetherall1@gmail.com>
2020-01-31 18:40:14 -0500

    logging

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -358,6 +358,7 @@ module.exports = {
 	},
 
 	async scoreMultipleTests(_, { input: { period, scoredTests } }, { studentData }) {
+		console.log(period, scoredTests)
 		scoredTests.forEach(test => {
 			studentData.updateOne(
 				{ _id: ObjectID(test._id), hasTests: { $elemMatch: { dueDate: test.dueDate } } },
