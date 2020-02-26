c672fbf9e6339d64bf0820e36d866b1bea33fc40
Rob Wetherall <robertgwetherall1@gmail.com>
2020-02-26 18:24:52 -0500

    more logging

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -449,9 +449,9 @@ module.exports = {
 		// })
 		// let students = await studentData.find({ _id: { $in: studentIds } })
 		// console.log(students)
-		const student = studentData.findOne({ _id: ObjectID(studentList[0]) })
+		const student = await studentData.findOne({ _id: ObjectID(studentList[0]) })
 		console.log(student)
-		return students
+		return null
 	},
 
 	async scoreMultipleTests(
