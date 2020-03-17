8a53eb03270a1d6f89ab47d4acc63a1e05e8d23e
Rob Wetherall <robertgwetherall1@gmail.com>
2020-03-17 13:41:33 -0400

    hope it  works

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function start() {
 			resave: false,
 			cookie: {
 				maxAge: 1000 * 60 * 60 * 24 * 7,
-				httpOnly: false,
+				httpOnly: false, //hope it works
 				sameSite: 'none',
 				secure: true
 			},
