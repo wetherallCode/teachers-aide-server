0314f38a592555bc833c7ef5be89ba39753c7b15
Rob Wetherall <robertgwetherall1@gmail.com>
2020-03-17 14:17:43 -0400

    does this work

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ async function start() {
 		resave: false,
 		cookie: {
 			maxAge: 1000 * 60 * 60 * 24 * 7,
-			sameSite: 'none'
+			sameSite: 'none',
+			httpOnly: false
 		},
 		store: store,
 		saveUninitialized: false
