77ca94c062f207515ddb14d2478702e6b88ee039
Rob Wetherall <robertgwetherall1@gmail.com>
2020-03-17 13:19:46 -0400

    added sameSite:none and secure:true to the login cookie

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ async function start() {
 			secret: process.env.SESSION_SECRET,
 			resave: false,
 			cookie: {
-				maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+				maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+				sameSite: 'none',
+				secure: true
 			},
+
 			store: store,
 			saveUninitialized: false
 		})
