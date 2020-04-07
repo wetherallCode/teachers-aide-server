0048216468804d5ea1ad8b0734e5dd1a814a90c1
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-04-07 10:09:09 -0400

    adding JSON

--- resolvers/Type.js
+++ resolvers/Type.js
@@ -1,26 +1,28 @@
 const { GraphQLScalarType } = require('graphql')
+const GraphQLJSON = require('graphql-type-json')
 
 module.exports = {
-	Date: new GraphQLScalarType({
-		name: 'Date',
-		description: 'A valid date value',
-		serialize: value => value.substring(0, 10),
-		parseValue: value => new Date(value).toISOString().substring(0, 10),
-		parseLiteral: literal => new Date(literal.value).toISOString().substring(0, 10)
-	}),
-
-	Protocol: {
-		__resolveType: parent => {
-			if (parent.socraticQuestion) {
-				return 'SocraticQuestionProtocol'
-			}
-		}
-	},
-	ClassProtocols: {
-		__resolveType: parent => {
-			if (parent.socraticQuestion) {
-				return 'SocraticQuestionProtocolForClassPeriod'
-			}
-		}
-	}
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'A valid date value',
+    serialize: value => value.substring(0, 10),
+    parseValue: value => new Date(value).toISOString().substring(0, 10),
+    parseLiteral: literal =>
+      new Date(literal.value).toISOString().substring(0, 10)
+  }),
+  JSON: GraphQLJSON,
+  Protocol: {
+    __resolveType: parent => {
+      if (parent.socraticQuestion) {
+        return 'SocraticQuestionProtocol'
+      }
+    }
+  },
+  ClassProtocols: {
+    __resolveType: parent => {
+      if (parent.socraticQuestion) {
+        return 'SocraticQuestionProtocolForClassPeriod'
+      }
+    }
+  }
 }
